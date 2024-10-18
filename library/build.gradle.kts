8979554b35d5a9c51fa83333eb356b636ee0f788
pterror <pterrorbird@gmail.com>
2024-10-19 04:01:09 +1000

    feat: add graalvm dependencies

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,1 +1,13 @@
 plugins { id("buildlogic.kotlin-library-conventions") }
+
+dependencies {
+  implementation("org.graalvm.polyglot:polyglot:24.1.1")
+  implementation("org.graalvm.polyglot:js:24.1.1")
+  implementation("org.graalvm.polyglot:python:24.1.1")
+  implementation("org.graalvm.polyglot:java:24.1.1")
+  implementation("org.graalvm.polyglot:ruby:24.1.1")
+  implementation("org.graalvm.polyglot:wasm:24.1.1")
+  implementation("org.graalvm.polyglot:llvm:24.1.1")
+  implementation("org.graalvm.polyglot:llvm-native:24.1.1")
+  implementation("org.graalvm.ruby:ruby:24.1.1")
+}

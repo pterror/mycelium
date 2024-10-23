24aeffc9ebc120c098cc6dbe5455bccce0dd2428
pterror <pterrorbird@gmail.com>
2024-10-23 14:18:26 +1000

    fix: fix `HttpChannel` implementation

--- library/src/main/kotlin/org/mycelium/library/Mycelium.kt
+++ library/src/main/kotlin/org/mycelium/library/Mycelium.kt
@@ -1,9 +1,9 @@
 package org.mycelium.library
 
 import java.io.File
-import java.net.URI
-import java.net.URL
 import java.net.HttpURLConnection
+import java.net.URI
+import java.nio.ByteBuffer
 import java.nio.channels.SeekableByteChannel
 import java.nio.file.AccessMode
 import java.nio.file.DirectoryStream.Filter
@@ -11,7 +11,8 @@ import java.nio.file.LinkOption
 import java.nio.file.OpenOption
 import java.nio.file.Path
 import java.nio.file.attribute.FileAttribute
-import java.nio.ByteBuffer
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.text.startsWith
 import org.graalvm.polyglot.Context
 import org.graalvm.polyglot.Source
@@ -19,14 +20,19 @@ import org.graalvm.polyglot.io.FileSystem
 import org.graalvm.polyglot.io.IOAccess
 
 class Mycelium {
+    val filesystem = InterceptingFileSystem()
     // `allowIO` is required for imports to work
     val context =
             Context.newBuilder()
                     // Enable `Polyglot` and `Java` globals
                     .allowAllAccess(true)
                     // Intercept file system calls (used to support HTTP imports)
-                    .allowIO(IOAccess.newBuilder().fileSystem(InterceptingFileSystem()).build())
+                    .allowIO(IOAccess.newBuilder().fileSystem(filesystem).build())
                     .build()
+
+    init {
+        filesystem.init(context)
+    }
 
     fun runString(language: String, code: String) {
         context.eval(language, code)
@@ -50,9 +56,10 @@ class Mycelium {
             }
 }
 
-class HttpChannel(val uri: URI): SeekableByteChannel {
+class HttpChannel(val uri: URI) : SeekableByteChannel {
     private final val connection = uri.toURL().openConnection() as HttpURLConnection
-    private final val buffer = ByteBuffer.allocate(0x10000)
+    private var buffer = ByteBuffer.allocate(8192)
+    private var length = 0
 
     init {
         // Some websites send special text-only output to `curl`.
@@ -66,11 +73,53 @@ class HttpChannel(val uri: URI): SeekableByteChannel {
 
     override fun isOpen(): Boolean = false
 
-    override fun read(p0: ByteBuffer): Int = connection.inputStream.read(p0.array())
+    private fun ensure(capacity: Int) {
+        while (capacity > buffer.capacity()) {
+            val oldBuffer = buffer
+            buffer = ByteBuffer.allocate(buffer.capacity() * 2)
+            buffer.put(oldBuffer.array(), 0, length)
+            buffer.position(oldBuffer.position())
+        }
+    }
+
+    private fun fetchMoreData() {
+        if (connection.inputStream.available() == 0) return
+        val end = length + connection.inputStream.available()
+        if (end > length) {
+            ensure(end)
+            val position = buffer.position()
+            buffer.position(length)
+            val bytesRead =
+                    connection.inputStream.read(
+                            buffer.array(),
+                            buffer.arrayOffset(),
+                            buffer.capacity() - buffer.arrayOffset()
+                    )
+            length += max(0, bytesRead)
+            buffer.position(position)
+        }
+    }
+
+    override fun read(p0: ByteBuffer): Int {
+        val end = buffer.position() + p0.limit()
+        if (end > length) fetchMoreData()
+        val bytesRead = min(p0.limit() - p0.arrayOffset(), length - buffer.position())
+        println("bytesRead=$bytesRead pos=${buffer.position()} end=$end length=$length ${buffer.position()}")
+        buffer.get(p0.array(), p0.arrayOffset(), bytesRead)
+        println(p0.array().toString(Charsets.UTF_8))
+        return if (bytesRead == 0) -1 else bytesRead
+    }
 
     override fun write(p0: ByteBuffer): Int {
-        connection.outputStream.write(p0.array())
-        return p0.limit()
+        val end = buffer.position() + p0.limit()
+        ensure(end)
+        val position = buffer.position()
+        buffer.position(length)
+        val bytesRead = p0.limit() - p0.arrayOffset()
+        buffer.put(p0.array(), p0.arrayOffset(), bytesRead)
+        length += bytesRead
+        buffer.position(position)
+        return p0.array().size
     }
 
     override fun position(): Long = buffer.position().toLong()
@@ -80,16 +129,29 @@ class HttpChannel(val uri: URI): SeekableByteChannel {
         return this
     }
 
-    override fun size(): Long = buffer.limit().toLong()
+    override fun size(): Long {
+        // TODO: Module execution fails when split into two reads.
+        // Remove `fetchMoreData()` below to see the error.
+        if (buffer.position() == length) fetchMoreData()
+        return length.toLong()
+    }
 
-    override fun truncate(p0: Long): SeekableByteChannel { 
-        buffer.limit(p0.toInt())
+    override fun truncate(p0: Long): SeekableByteChannel {
+        println("a")
+        length = p0.toInt()
+        buffer.limit(length)
+        buffer.compact()
         return this
     }
 }
 
 class InterceptingFileSystem : FileSystem {
+    private final var context: Context? = null
     private final val delegate = FileSystem.newDefaultFileSystem()
+
+    fun init(context: Context) {
+        this.context = context
+    }
 
     override fun parsePath(uri: URI) =
             if (uri.toString().startsWith('/')) Path.of(uri) else Path.of("/.mycelium/" + uri)
